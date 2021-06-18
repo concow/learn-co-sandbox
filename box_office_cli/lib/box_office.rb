4237ff021efcdaca245377a4ad49a3d366b6b390
Connor Cowan <ccowan003@comcast.net>
2021-06-18 22:00:24 +0000

    Automatically backed up by Learn

--- box_office_cli/lib/box_office.rb
+++
@@ -1,8 +0,0 @@
-require_relative "box_office_cli/version"
-
-module BoxOfficeCli
-  class Error < StandardError; end
-  # Your code goes here...
-end
-
-require_relative './box_office/movie'
--- box_office_cli/lib/box_office/version.rb
+++
@@ -1,3 +0,0 @@
-module BoxOfficeCli
-  VERSION = "0.1.0"
-end
